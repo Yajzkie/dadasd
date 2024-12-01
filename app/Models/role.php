14182333e3c792cf400a675472d8ec323483f90b
Yajzkie <yaj143sabalo@gmail.com>
2024-12-01 22:42:00 +0800

    fixed role model

--- app/Models/role.php
+++ app/Models/role.php
@@ -11,8 +11,8 @@ class Role extends Model
     use HasFactory;
 
     public function users()
-{
-    return $this->belongsToMany(User::class, 'role_id');
-}
+    {
+        return $this->belongsToMany(User::class, 'role_id');
+    }
 
 }
