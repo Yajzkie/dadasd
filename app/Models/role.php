79701c6bb83de14351914d3bf4917e01edf01fb0
Yajzkie <yaj143sabalo@gmail.com>
2024-12-01 22:39:28 +0800

    fixed role model

--- app/Models/role.php
+++ app/Models/role.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
-class role extends Model
+class Role extends Model
 {
     use HasFactory;
 
